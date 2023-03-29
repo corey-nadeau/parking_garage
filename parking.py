2fc850810354d328ef6d4914c38e16ed771d02b6
corey.nadeau <djcjn16@hotmail.com>
2023-03-29 10:25:58 -0500

    third commit

--- parking.py
+++ parking.py
@@ -1,51 +1,58 @@
 class Garage():
-    def __init__(self,capacity,cars):
-        self.capacity = capacity
-        self.cars = cars
-    
-    def showCars(self):
-        print("Here are all of the cars in the garage: ")
-        for i in self.cars:
-            print(i)
+    def __init__(self):
+        self.tickets = 20
+        self.spaces = 20
+        self.currentTicket = {'owed':[], 'paid':[]}
+        self.dailyProfit = []
 
     def addCar(self):
+        if self.spaces < 1:
+            print('Garage full. Please come back another time.')
         carIn = input("What is your lisence plate number?: ")
-        self.cars.append(carIn)
+        id = carIn.lower()
+        self.currentTicket['owed'].append(id)
+        self.tickets = self.tickets -1
+        self.spaces = self.spaces -1
+        print(f'Please proceed to nearest available space. ({self.tickets} spaces remaining.)')
 
     def removeCar(self):
         carOut = input("What is your lisence plate number?: ")
-        self.cars.remove(carOut)
-        print("Thank you, have a nice day")
-
-myGarage = Garage(20,[])
-
-def run():
-    while True:
-        tickets = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-        parking_spaces = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t']
-        currentTicket = {}
-
-        response = input("Are you entering? or Leaving? : ")
-        if response.lower() == "leaving":
+        idOut = carOut.lower()
+        if idOut in self.currentTicket['owed']:
             hours = input("How many hours were you here rounded to nearest hour?: ")
             cost = int(hours)* 5
             stCost = str(cost)
             costIn = input("Your total cost is $"+ stCost + " please enter that amount now: ")
             if costIn == stCost:
-                myGarage.removeCar()
-                tickets.append(currentTicket[0])
-                
-            else:
-                print("please enter $"+ stCost + " as a whole integer: ")
-            
-        elif response.lower() == "show":
-            myGarage.showCars()
-                
-        else:
+                self.dailyProfit.append(costIn)
+                self.currentTicket['owed'].remove(idOut)
+                self.currentTicket['paid'].append(idOut)
+                self.tickets - self.tickets + 1
+                self.spaces = self.spaces + 1
+                print('Thank you, you have 15 minutes to leave. Have a nice day!')
+            else: 
+                print("please enter $"+ stCost + " as a whole integer: ")   
+        elif idOut not in self.currentTicket['owed']:
+            print('License plate number not found. Please try again. ')            
+
+myGarage = Garage()
+
+def run():
+    while True:
+
+        response = input("Are you Entering? or Leaving? : ")
+        if response.lower() == "leaving":
+            myGarage.removeCar()
+        elif response.lower() == "entering":
             myGarage.addCar()
-            print("Here is your ticket, please keep track of the time that you are parked here. ")
-            currentTicket.append(tickets[0])
-            
+        
+#  admin input only
+        elif response.lower() == "showprofit":
+            ints = []
+            for i in myGarage.dailyProfit:
+                ints.append(int(i))
+            print(f'Total amount collected today: ${sum(ints)}')
+
                     
 
 
